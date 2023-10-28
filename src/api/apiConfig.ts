264f43af9c1b9764d7c155023acf40dcc72f418c
Pablo Marrero <96428954+marreropd@users.noreply.github.com>
2023-10-28 13:00:11 +0200

    Update apiConfig.ts
    delete commented code

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,7 +2,5 @@ import axios from 'axios';
 
 export const apiConfig = axios.create({
 	baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
-	// headers: {
-	// 	Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
-	// },
+
 });
